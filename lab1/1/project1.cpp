8fe6e8dbb0ac901607311dcdc547b6c20040d647
Jan Kopec <jankopec7@gmail.com>
2020-10-26 10:16:10 +0100

    added file 1

--- lab1/1/project1.cpp
+++ lab1/1/project1.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+void printArray(int arr[], int size){  
+
+    for (int i = 0; i < size; i++)  
+        cout << arr[i] << " ";  
+    cout << endl;  
+}  
+
 void swap(int *x, int *y)  
 {  
     int temp = *x;  
@@ -9,24 +16,29 @@ void swap(int *x, int *y)
     *y = temp;  
 } 
 
-void bubbleSort(int array[], int n){  
+void bubbleSort(int array[], int n){
 
-    for (int i = 0; i < n-1; i++)      
+    for (int i = 1; i < n; i++)      
         for (int j = 0; j < n-i-1; j++)  
             if (array[j] > array[j+1])  
                 swap(&array[j], &array[j+1]);  
-}  
+}
 
-int main(){
+int main(int argc, char **argv){
 
-    int array[] = {100, 2, 30, 5, 8, 34, 56, 78, 90};
-    int n = sizeof(array)/sizeof(array[0]); 
+    int *array = new int[argc - 1];
 
-    bubbleSort(array, n);
-    cout << "Posortowana tablica: \n";  
-    for (int i = 0; i < n; i++){  
-        cout << array[i] << " ";  
-        cout << endl;  
-    }
+    for (int i = 0; i < argc - 1; i++)
+        array[i] = atoi(argv[i + 1]);
+
+    cout << "sort.exe ";
+    printArray(array, argc - 1); 
+
+    bubbleSort(array, argc);
+
+    cout << "out = ";
+    printArray(array, argc - 1); 
+
+    delete[] array;
     return 0;
 }
